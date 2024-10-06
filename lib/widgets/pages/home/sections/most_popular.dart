54637df88f71197704733150f583f76b905a7e36
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-10-06 13:19:13 +0530

    Added basic layout for most popular section in homepage (without product cards as of now)

--- lib/widgets/pages/home/sections/most_popular.dart
+++ lib/widgets/pages/home/sections/most_popular.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'package:attic_mobile/widgets/pages/home/section.dart';
 
 class HomePageMostPopularSection extends StatelessWidget {
   const HomePageMostPopularSection({super.key});
 
+  Widget _buildViewAllButton() {
+    return [
+      "View All".text.size(12).semiBold.gray600.make(),
+      2.widthBox,
+      "(20)".text.size(12).gray400.make(),
+    ].row().onTap(() {
+      print("Tapped on View All");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return [
+      HomePageSection(title: "Most Popular", child: _buildViewAllButton()),
+    ].vStack(crossAlignment: CrossAxisAlignment.start);
   }
 }
