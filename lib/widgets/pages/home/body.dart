49ab4ee899c3e655ff9fb71521d819d2f62c2d47
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-10-06 19:49:37 +0530

    Added widget for new arrivals section in homepage

--- lib/widgets/pages/home/body.dart
+++ lib/widgets/pages/home/body.dart
@@ -1,4 +1,5 @@
 import 'package:attic_mobile/widgets/pages/home/sections/most_popular.dart';
+import 'package:attic_mobile/widgets/pages/home/sections/new_arrivals.dart';
 import 'package:attic_mobile/widgets/pages/home/sections/popular_brands.dart';
 import 'package:attic_mobile/widgets/pages/home/sections/spotlight.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,8 @@ class HomePageBody extends StatelessWidget {
         const HomePagePopularBrandsSection(),
         36.heightBox,
         const HomePageMostPopularSection(),
+        36.heightBox,
+        const HomePageNewArrivalsSection(),
       ],
     ).p16().scrollVertical(controller: scrollController);
   }
---
+++ lib/widgets/pages/home/sections/new_arrivals.dart
@@ -0,0 +1,30 @@
+import 'package:attic_mobile/widgets/common/product_grid.dart';
+import 'package:attic_mobile/widgets/pages/home/common/section.dart';
+import 'package:attic_mobile/widgets/pages/lib/misc.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePageNewArrivalsSection extends StatelessWidget {
+  const HomePageNewArrivalsSection({super.key});
+
+  Widget _buildViewAllButton() {
+    return [
+      "View All".text.size(12).semiBold.gray600.make(),
+      2.widthBox,
+      "(20)".text.size(12).gray400.make(),
+    ].row().onTap(() {
+      print("Tapped on View All");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return [
+      HomePageSection(title: "New Arrivals", child: _buildViewAllButton()),
+      ProductGrid(
+        crossAxisCount: 1,
+        products: [product, product, product, product],
+      ),
+    ].vStack(crossAlignment: CrossAxisAlignment.start);
+  }
+}
