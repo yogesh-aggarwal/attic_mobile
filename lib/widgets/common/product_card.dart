4c1741b3650fde5e131bbcf0e7af56c59a878f21
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-10-06 19:46:27 +0530

    Minor changes

--- lib/widgets/common/product_card.dart
+++ lib/widgets/common/product_card.dart
@@ -69,67 +69,71 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(width: 1.0, color: Colors.grey.shade200),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.08),
-            spreadRadius: 0,
-            blurRadius: 15.0,
-          ),
-        ],
+    return InkWell(
+      onTap: () {},
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(width: 1.0, color: Colors.grey.shade200),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              spreadRadius: 0,
+              blurRadius: 15.0,
+            ),
+          ],
+        ),
+        child: [
+          // Image
+          _buildImage().p4(),
+          8.heightBox,
+          // Row 1
+          [
+            // Title
+            product.title.text.size(15).semiBold.ellipsis.make().flexible(),
+            8.widthBox,
+          ].hStack().px12(),
+          2.heightBox,
+          // Row 2
+          [
+            // Brand name
+            [
+              "Adidas".text.size(14).gray400.make(),
+              4.widthBox,
+              Icon(Icons.check_circle, size: 16, color: Colors.blue.shade800),
+            ].hStack(),
+            // Star rating
+            [
+              Icon(Icons.star, size: 16, color: Colors.yellow.shade800),
+              3.widthBox,
+              product.stats.rating.toString().text.size(14).semiBold.make(),
+            ].hStack(),
+          ].hStack(alignment: MainAxisAlignment.spaceBetween).px12(),
+          8.heightBox,
+          // Row 3
+          [
+            // Price
+            "INR ${product.costing.sellingPrice.round()}.99"
+                .toString()
+                .text
+                .size(14)
+                .semiBold
+                .make(),
+            12.widthBox,
+            // Discount
+            "${product.details.stock} in stock"
+                .text
+                .size(12)
+                .ellipsis
+                .gray400
+                .make()
+                .expand(),
+          ].hStack(alignment: MainAxisAlignment.spaceBetween).px12(),
+          14.heightBox,
+        ].vStack(crossAlignment: CrossAxisAlignment.start),
       ),
-      child: [
-        // Image
-        _buildImage().p4(),
-        8.heightBox,
-        // Row 1
-        [
-          // Title
-          product.title.text.size(15).semiBold.ellipsis.make().flexible(),
-          8.widthBox,
-        ].hStack().px12(),
-        2.heightBox,
-        // Row 2
-        [
-          // Brand name
-          [
-            "Adidas".text.size(14).gray400.make(),
-            4.widthBox,
-            Icon(Icons.check_circle, size: 16, color: Colors.blue.shade800),
-          ].hStack(),
-          // Star rating
-          [
-            Icon(Icons.star, size: 16, color: Colors.yellow.shade800),
-            3.widthBox,
-            product.stats.rating.toString().text.size(14).semiBold.make(),
-          ].hStack(),
-        ].hStack(alignment: MainAxisAlignment.spaceBetween).px12(),
-        8.heightBox,
-        // Row 3
-        [
-          // Price
-          "INR ${product.costing.sellingPrice.round()}.99"
-              .toString()
-              .text
-              .size(14)
-              .semiBold
-              .make(),
-          12.widthBox,
-          // Discount
-          "${product.details.stock} in stock"
-              .text
-              .size(12)
-              .ellipsis
-              .gray400
-              .make()
-              .expand(),
-        ].hStack(alignment: MainAxisAlignment.spaceBetween).px12(),
-        14.heightBox,
-      ].vStack(crossAlignment: CrossAxisAlignment.start),
     );
   }
 }
